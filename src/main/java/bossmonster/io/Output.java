0502edc7f22d1c995fcfe52151c470636f3b5fcc
aka-nick <bechalrang@gmail.com>
2023-10-03 14:21:51 +0900

    추가( Input.java ) : 출력 클래스 작성

---
+++ src/main/java/bossmonster/io/Output.java
@@ -0,0 +1,12 @@
+package bossmonster.io;
+
+public class Output {
+
+    public static void println(String message) {
+        System.out.println(message);
+    }
+    public static void print(String message) {
+        System.out.print(message);
+    }
+
+}
